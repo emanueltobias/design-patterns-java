0fc7f9f7ffe73217eb943430e910d5bd37c7bf5b
Emanuel Tobias <emanuel_tobias_@hotmail.com>
2022-11-24 22:21:25 -0300

    Padrão criacional Prototype

---
+++ creational-patterns/src/main/java/com/emanueltobias/prototype/functionalCloneBuilder/Client.java
@@ -0,0 +1,21 @@
+package com.emanueltobias.prototype.functionalCloneBuilder;
+
+import com.emanueltobias.prototype.functionalCloneBuilder.model.Address;
+import com.emanueltobias.prototype.functionalCloneBuilder.model.User;
+
+public class Client {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		User user = new User("ToClone", 25, new Address("ABC Street", 1000));
+		System.out.println(user);
+		
+		User cloneUser = user.cloneBuilder().withName("Cloned++").now();
+		System.out.println(cloneUser);
+		
+		User anotherUser = cloneUser.cloneBuilder()
+				.withAge(100)
+				.withAddress(new Address("A Street", 500))
+				.now();
+		System.out.println(anotherUser);
+	}
+}
